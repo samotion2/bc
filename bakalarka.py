74fe69f9a9a8dd21160190774d32eb73b98b943b
unknown <uplnyrandom@gmail.com>
2022-02-19 20:38:38 +0100

    generate button added

--- bakalarka.py
+++ bakalarka.py
@@ -42,16 +42,68 @@ def crtscatter(arr, color, data, ax):
     sct = ax.scatter(x, y, zorder=1, alpha= 0.2, c=color, cmap=mpl.cm.rainbow, s=30, visible=False)
     arr.append(sct)
 
-def generateclick(event):
+def hidecb():
+    global cbpresent
+    global cb
+
+    if cbpresent == True:
+        cb.remove()
+        cbpresent = False
+
+
+def generateclick(ax, checkbox, labels, event):
     #vyber suboru
     tkinter.Tk().withdraw()
     filename = askopenfilename(title='Choose your file', filetypes=[("csvs", (".txt", ".log", "csv")), ("all", "*")])
-    #print(filename)
-    
+
     if not filename:
         print('You have to choose a file first!')
+        return
     
-    return filename
+    checkbox.set_active(0)
+    hidecb()
+    ax.clear()
+    switch = [0]
+    
+    try:
+        data = pd.read_csv(filename, names=['TIME','2','LATITUDE','4','LONGITUDE','6','HMSL','8','GSPEED','10','CRS','12', 'HACC'], sep=';')
+    except:
+        print('zly file')
+        return
+    
+    x1, x2, y1, y2 = bbset(data)
+
+    #nastavenie colormapy
+    color = ['Blue', data['HMSL'], data['GSPEED'], data['CRS'], data['HACC']]
+
+    p = []
+    for i in color:
+        crtscatter(p, i, data, ax)
+
+    #print(p)
+    p[0].set_visible(True)
+
+    mplcursors.cursor(ax, hover=2).connect("add", lambda sel: sel.annotation.set_text(annot_format(sel, data, switch[0]))) #tooltip
+
+    checkbox.on_clicked(partial(radioclick, p, data, ax, switch, labels))
+
+    #nastavenie hranic tabulky
+    ax.set_title(filename)
+    ax.set_xlim(x1, x2)
+    ax.set_ylim(y1, y2)
+
+    #formatovanie osi tabulky
+    ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.4f}'))
+    ax.xaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.4f}'))
+
+    extent = tilemapbase.Extent.from_lonlat(x1,x2,y1,y2)     #tu 4 veci <-----------------------------------------------
+    t = tilemapbase.tiles.build_OSM()
+
+    plotter = tilemapbase.Plotter(extent, t, width=200)
+    plotter.plot(ax)
+
+    plt.subplots_adjust(left=0.25, top= 0.95, bottom= 0.05)
+
 
 def radioclick(p, data, ax, switch, labels, label):
     global cbpresent
@@ -69,9 +121,7 @@ def radioclick(p, data, ax, switch, labels, label):
     # if i > 0:
     #     cb.remove()
 
-    if cbpresent == True:
-        cb.remove()
-        cbpresent = False
+    hidecb()
 
     if label == 'Speed':
         vmin=data['GSPEED'].min()*3.6/100
@@ -112,7 +162,8 @@ def bbset(data):
 
 def init(i):
     if i==1:
-        data_path = '.\\averaged.txt'
+        #data_path = '\\averaged.txt'
+        data_path = 'C:\\Users\\PC\\Desktop\\averaged.txt'
         #data_path = '.\\sobota_log\\dole\\2021-04-10_08-52-05_gps.log'
         #data_path = '.\\nedela_log\\dole\\2021-04-11_13-36-28_gps.log'
         img_path = 'koliba.png'
@@ -132,57 +183,7 @@ def init(i):
     return data_path, x1, x2, y1, y2, img_path
 
 def main():
-    switch = [0]
-    data_path, x1, x2, y1, y2, img_path = init(1) #1-koliba, 0-zilina
-
-    #data_path = generateclick()
-    
-    try:
-        data = pd.read_csv(data_path, names=['TIME','2','LATITUDE','4','LONGITUDE','6','HMSL','8','GSPEED','10','CRS','12', 'HACC'], sep=';')
-    except:
-        print('zly file')
-        return
-    #print(data.head())
-    #bbset(data)
-    
-    x1, x2, y1, y2 = bbset(data)
-
-
     fig, ax = plt.subplots(figsize = (11,7))
-
-    #nastavenie colormapy
-    color = ['Blue', data['HMSL'], data['GSPEED'], data['CRS'], data['HACC']]
-    p = []
-    #p.append(ax.scatter(data['LONGITUDE']/10000000, data['LATITUDE']/10000000, zorder=1, alpha= 0.2, s=30, visible=True))
-    for i in color:
-        crtscatter(p, i, data, ax)
-
-    p[0].set_visible(True)
-    cursor = mplcursors.cursor(ax, hover=True) #tooltip
-    @cursor.connect("add")
-    def on_add(sel):
-        sel.annotation.set(text = annot_format(sel, data, switch[0]))
-
-    #nastavenie hranic tabulky
-    ax.set_title(data_path)
-    ax.set_xlim(x1, x2)
-    ax.set_ylim(y1, y2)
-
-    #formatovanie osi tabulky
-    ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.4f}'))
-    ax.xaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.4f}'))
-
-    #nacitanie mapy na pozadi
-    # img = plt.imread(img_path)
-    # ax.imshow(img, zorder= 0, extent = [x1, x2, y1, y2], aspect= 'equal')
-
-    extent = tilemapbase.Extent.from_lonlat(x1,x2,y1,y2)     #tu 4 veci <-----------------------------------------------
-    t = tilemapbase.tiles.build_OSM()
-    
-    plotter = tilemapbase.Plotter(extent, t, width=200)
-    plotter.plot(ax)
-
-    plt.subplots_adjust(left=0.25, top= 0.95, bottom= 0.05)
 
     #radiobuttons
     labels = ['GPS', 'Altitude', 'Speed', 'Course', 'HACC']
@@ -192,12 +193,9 @@ def main():
     axButton = plt.axes([0.01,0.7,0.10,0.08]) 
     button = Button(axButton, "Generate")
 
-    checkbox.on_clicked(partial(radioclick, p, data, ax, switch, labels))
-    button.on_clicked(generateclick)
-    # print(data['HACC'].min())
-    # print(data['HACC'].max())
-
-    # print(data['HMSL']/1000)
+    button.on_clicked(partial(generateclick, ax, checkbox, labels))
+    
+    plt.subplots_adjust(left=0.3)
     plt.show()
 
 if __name__ == '__main__':
